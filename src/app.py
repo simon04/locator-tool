4ed3c71507d3b857e02621086e5992df335a13ad
Simon Legner <Simon.Legner@gmail.com>
2016-05-01 14:27:50 +0000

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,34 @@
+import configparser
+from flask import Flask, jsonify, request
+from flask_mwoauth import MWOAuth
+
+config = configparser.ConfigParser()
+config.read('../config.ini')
+
+app = Flask(__name__)
+app.secret_key = config.get('auth', 'secret_key')
+
+mwoauth = MWOAuth(base_url='https://commons.wikimedia.org/w',
+                  clean_url='https://commons.wikimedia.org/wiki',
+                  consumer_key=config.get('auth', 'consumer_key'),
+                  consumer_secret=config.get('auth', 'consumer_secret'))
+app.register_blueprint(mwoauth.bp)
+
+@app.route("/")
+def index():
+    return "logged in as: " + repr(mwoauth.get_current_user(False)) + "<br>" + \
+           "<a href=login>login</a> / <a href=logout>logout</a>"
+
+@app.route("/user")
+def user():
+    r = jsonify(user=mwoauth.get_current_user(False))
+    return (r, 200, {'Access-Control-Allow-Origin': 'http://localhost:8000/'})
+
+@app.route("/query")
+def query():
+    q = {'action': 'query', 'prop': request.args.get('prop'), 'titles': request.args.get('titles')}
+    r = jsonify(mwoauth.request(q))
+    return (r, 200, {'Access-Control-Allow-Origin': 'http://localhost:8000/'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
