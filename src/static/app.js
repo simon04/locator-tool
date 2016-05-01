9cd7e629e1c09f144ba81c578a9c30072e63ea7a
Simon Legner <Simon.Legner@gmail.com>
2016-05-01 19:27:32 +0000

    Fetch files from category

--- src/static/app.js
+++ src/static/app.js
@@ -1,5 +1,23 @@
 angular.module('app', ['ui.router']);
 
+angular.module('app').config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider.state('input',{
+    url: '/input',
+    templateUrl: 'partials/input.html',
+    controllerAs: '$ctrl',
+    controller: function(ltData) {
+      var vm = this;
+      vm.getFilesForCategory = function() {
+        ltData.getFilesForCategory(vm.category).then(function(files) {
+          vm.titles = files && files.join('\n');
+        });
+      }
+    }
+  });
+
+});
+
 angular.module('app').controller('Ctrl', function($scope, $http) {
   $http.get('/locator-tool/query', {params: {
     prop: 'coordinates',
@@ -9,11 +27,16 @@ angular.module('app').controller('Ctrl', function($scope, $http) {
   });
 });
 
-angular.module('app').factory('ltUserData', function($http) {
+angular.module('app').factory('ltData', function($http) {
   return {
     getUserInfo: function() {
       return $http.get('/locator-tool/user').then(function(d) {
         return d.data && d.data.user;
+      });
+    },
+    getFilesForCategory: function(cat) {
+      return $http.get('/cats-php/', {params: {lang: 'commons', type: 6, cat: cat}}).then(function(d) {
+        return d.data;
       });
     }
   };
@@ -23,9 +46,9 @@ angular.module('app').component('ltUserInfo', {
   template: [
     '<a ng-hide="$ctrl.user" class="btn btn-default navbar-btn">Sign in</a>',
     '<p ng-show="$ctrl.user" class="navbar-text">Signed in as {{$ctrl.user}}</p>'].join(''),
-  controller: function(ltUserData) {
+  controller: function(ltData) {
     var vm = this;
-    ltUserData.getUserInfo().then(function(user) {
+    ltData.getUserInfo().then(function(user) {
       vm.user = user;
     });
   }
