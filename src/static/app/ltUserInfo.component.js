b853d95aa7b3ee0b6e9dce6127c582c90bef4b40
Simon Legner <Simon.Legner@gmail.com>
2016-05-16 18:00:27 +0200

    Redirect after login/logout

--- src/static/app/ltUserInfo.component.js
+++ src/static/app/ltUserInfo.component.js
@@ -1,8 +1,9 @@
 angular.module('app').component('ltUserInfo', {
+  /* eslint max-len: 0 */
   template: [
-    '<a href="/locator-tool/login" ng-hide="$ctrl.user" class="btn btn-default navbar-btn">Sign in</a>',
+    '<a href="/locator-tool/login?next=index.html" ng-hide="$ctrl.user" class="btn btn-default navbar-btn">Sign in</a>',
     '<p ng-show="$ctrl.user" class="navbar-text">Signed in as {{$ctrl.user}}</p>',
-    '<a href="/locator-tool/logout" ng-show="$ctrl.user" class="btn btn-default navbar-btn">Sign out</a>'
+    '<a href="/locator-tool/logout?next=index.html" ng-show="$ctrl.user" class="btn btn-default navbar-btn">Sign out</a>'
   ].join(''),
   controller: function(ltDataAuth) {
     var vm = this;
