ea370da032df7eb65ee84c056a9d7a55fedd0f19
Simon Legner <Simon.Legner@gmail.com>
2016-05-26 22:45:34 +0200

    Show how many files to geolocate

--- src/static/app/input/input.controller.js
+++ src/static/app/input/input.controller.js
@@ -1,27 +1,34 @@
 angular.module('app').controller('InputController', function(ltData, $state) {
   var vm = this;
+  vm.titles = '';
   vm.getCategoriesForPrefix = function() {
     ltData.getCategoriesForPrefix(vm.category).then(function(categories) {
       vm.categorySuggestions = categories;
     });
   };
   vm.getFilesForUser = function() {
-    ltData.getFilesForUser(vm.user).then(function(files) {
-      vm.titles = files && files.join('\n');
-    });
+    ltData.getFilesForUser(vm.user).then(setTitles);
   };
   vm.getFilesForCategory = function() {
-    ltData.getFilesForCategory(vm.category).then(function(files) {
-      vm.titles = files && files.join('\n');
-    });
+    ltData.getFilesForCategory(vm.category).then(setTitles);
   };
   vm.next = function() {
-    $state.go('list', {titles: getTitles().join('|')});
+    $state.go('list', {titles: vm.titleList.join('|')});
   };
 
-  function getTitles() {
-    return vm.titles.split('\n').map(function(file) {
-      return file && file.split('|')[0];
-    });
+  Object.defineProperty(vm, 'titleList', {
+    get: function() {
+      return vm.titles.split('\n').map(function(file) {
+        return file && file.split('|')[0];
+      }).filter(angular.identity);
+    },
+    set: function(files) {
+      vm.titles = files && files.join('\n');
+    },
+    enumerable: true,
+    configurable: true
+  });
+  function setTitles(files) {
+    vm.titleList = files;
   }
 });
