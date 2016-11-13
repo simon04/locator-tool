5aff6cadd13f7e9899cb57078f68db4d2f1e56b8
Simon Legner <Simon.Legner@gmail.com>
2016-11-13 13:28:20 +0100

    PEP8

--- src/location_to_wikitext.py
+++ src/location_to_wikitext.py
@@ -1,7 +1,11 @@
 import re
 
+
 def add_location_to_wikitext(lat, lng, wikitext):
     r"""
+    Adds a {{Location}} template containig `lat`, `lng` to the `wikitext`.
+    Replaces previously specified `{{Location}}`, `{{Location dec}}` templates.
+
     >>> add_location_to_wikitext(12.3, 45.6, "")
     '{{Location|12.3|45.6}}'
     >>> add_location_to_wikitext(12.3, 45.6, "{{Information}}{{Location|87.65|-43.21|region:XY-Z}}")
@@ -16,23 +20,25 @@ def add_location_to_wikitext(lat, lng, wikitext):
     '{{Location|12.3|45.6|region:FR}}\n'
     """
     location = "{{Location|%s|%s}}" % (lat, lng)
-    numericArg = r'(\|\s*[-+.0-9]+\s*)'
-    pattern = re.compile(r'\{\{\s*Location\s*(' + numericArg + '{3}\|\s*[NESW]\s*){2}', flags=re.IGNORECASE)
+    numeric_arg = r'(\|\s*[-+.0-9]+\s*)'
+    pattern = re.compile(
+        r'\{\{\s*Location\s*(' + numeric_arg + '{3}\|\s*[NESW]\s*){2}', flags=re.IGNORECASE)
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
-    pattern = re.compile(r'\{\{\s*Location( dec)?\s*' + numericArg + numericArg, flags=re.IGNORECASE)
+    pattern = re.compile(
+        r'\{\{\s*Location( dec)?\s*' + numeric_arg + numeric_arg, flags=re.IGNORECASE)
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
     pattern = re.compile(r'\{\{\s*Information', flags=re.IGNORECASE)
     match = pattern.search(wikitext)
     if match:
-        braceCount = 2
+        brace_count = 2
         pos = match.start() + 2
-        while braceCount > 0 and pos < len(wikitext):
+        while brace_count > 0 and pos < len(wikitext):
             if wikitext[pos] == '{':
-                braceCount = braceCount + 1
+                brace_count += 1
             elif wikitext[pos] == '}':
-                braceCount = braceCount - 1
-            pos = pos + 1
+                brace_count -= 1
+            pos += 1
         return '\n'.join([wikitext[:pos], location, wikitext[pos:]])
     return wikitext + location
