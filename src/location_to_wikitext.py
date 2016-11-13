094e210e9b570e2784ddeddea5c5c07ea5755356
Simon Legner <Simon.Legner@gmail.com>
2016-11-13 13:23:10 +0100

    Detect lowercase `{{location dec}}`
    Fixes #5.

--- src/location_to_wikitext.py
+++ src/location_to_wikitext.py
@@ -12,16 +12,18 @@ def add_location_to_wikitext(lat, lng, wikitext):
     'Foo\n{{Information|foo={{de|sdf}}|bar={{lic}}}}\n{{Location|12.3|45.6}}\nBar'
     >>> add_location_to_wikitext(12.3, 45.6, "X\n{{Location dec|50.917385|13.342268}}\nX")
     'X\n{{Location|12.3|45.6}}\nX'
+    >>> add_location_to_wikitext(12.3, 45.6, "{{location dec|43.599468|1.445375|region:FR}}\n")
+    '{{Location|12.3|45.6|region:FR}}\n'
     """
     location = "{{Location|%s|%s}}" % (lat, lng)
     numericArg = r'(\|\s*[-+.0-9]+\s*)'
-    pattern = re.compile(r'\{\{\s*Location\s*(' + numericArg + '{3}\|\s*[NESW]\s*){2}')
+    pattern = re.compile(r'\{\{\s*Location\s*(' + numericArg + '{3}\|\s*[NESW]\s*){2}', flags=re.IGNORECASE)
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
-    pattern = re.compile(r'\{\{\s*Location( dec)?\s*' + numericArg + numericArg)
+    pattern = re.compile(r'\{\{\s*Location( dec)?\s*' + numericArg + numericArg, flags=re.IGNORECASE)
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
-    pattern = re.compile(r'\{\{\s*Information')
+    pattern = re.compile(r'\{\{\s*Information', flags=re.IGNORECASE)
     match = pattern.search(wikitext)
     if match:
         braceCount = 2
