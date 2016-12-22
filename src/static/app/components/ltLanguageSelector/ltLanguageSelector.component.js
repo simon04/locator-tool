c967ed6344fa3a787e2af24a9726a0aeea7e07e6
Simon Legner <Simon.Legner@gmail.com>
2016-12-22 20:51:56 +0100

    ltLanguageSelector: auto detect, use localStorage

--- src/static/app/components/ltLanguageSelector/ltLanguageSelector.component.js
+++ src/static/app/components/ltLanguageSelector/ltLanguageSelector.component.js
@@ -1,18 +1,34 @@
 angular.module('app').component('ltLanguageSelector', {
   templateUrl: 'app/components/ltLanguageSelector/ltLanguageSelector.html',
-  controller: function(gettextCatalog) {
+  controller: function($window, localStorageService, gettextCatalog) {
     var vm = this;
     vm.languages = {
       en: 'English',
       de: 'German'
     };
+    init();
     Object.defineProperty(vm, 'language', {
       get: function() {
         return gettextCatalog.getCurrentLanguage();
       },
       set: function(lang) {
+        localStorageService.set('language', lang);
         gettextCatalog.setCurrentLanguage(lang);
       }
     });
+
+    function init() {
+      var language = localStorageService.get('language');
+      if (language) {
+        gettextCatalog.setCurrentLanguage(language);
+      } else if ($window.navigator && $window.navigator.languages) {
+        var langs = $window.navigator.languages.filter(function(lang) {
+          return vm.languages[lang];
+        });
+        if (langs.length) {
+          gettextCatalog.setCurrentLanguage(langs[0]);
+        }
+      }
+    }
   }
 });
