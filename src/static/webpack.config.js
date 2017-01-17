e68fb7a164bbc6122519326a4a0f69990fedb262
Simon Legner <Simon.Legner@gmail.com>
2017-01-17 13:07:55 +0100

    webpack: productionBuild constant

--- src/static/webpack.config.js
+++ src/static/webpack.config.js
@@ -4,13 +4,15 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const productionBuild = process.env.npm_lifecycle_script !== 'webpack-dev-server';
+
 module.exports = {
   entry: {
     main: './app/index.js',
     vendor: './app/vendor.js'
   },
   output: {
-    path: process.env.npm_lifecycle_script === 'webpack-dev-server' ? undefined : './dist',
+    path: productionBuild && './dist' || undefined,
     filename: '[name].[chunkhash].js',
     sourceMapFilename: '[name].[chunkhash].map'
   },
