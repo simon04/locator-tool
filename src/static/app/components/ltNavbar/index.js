9e6b46e03f88799e89f07adf16409369ed451cc9
Simon Legner <Simon.Legner@gmail.com>
2017-05-19 20:06:03 +0200

    ltNavbar: switch between geolocate and map view

--- src/static/app/components/ltNavbar/index.js
+++ src/static/app/components/ltNavbar/index.js
@@ -1,5 +1,20 @@
 import template from './ltNavbar.pug';
 
+class controller {
+  constructor($transitions) {
+    this.params = undefined;
+    $transitions.onSuccess({}, transition => {
+      this.params = transition.params();
+    });
+  }
+
+  get activateLinks() {
+    return this.params && (this.params.files || this.params.user || this.params.category);
+  }
+}
+controller.$inject = ['$transitions'];
+
 export default {
+  controller,
   template
 };
