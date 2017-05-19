4915258f5c7ed049767973b33047c4839dcf44cd
Simon Legner <Simon.Legner@gmail.com>
2017-05-19 20:48:20 +0200

    Refactoring (ltLocation Pug template)

--- src/static/app/components/ltLocation/index.js
+++ src/static/app/components/ltLocation/index.js
@@ -1,12 +1,9 @@
+import template from './ltLocation.pug';
+
 export default {
   bindings: {
     type: '@',
     latLng: '<'
   },
-  template: [
-    '<code>{{',
-    '<span>{{$ctrl.type || "Location"}}</span>',
-    '<span ng-if="$ctrl.latLng"> | {{$ctrl.latLng.lat}} | {{$ctrl.latLng.lng}}</span>',
-    '}}</code>'
-  ].join('')
+  template
 };
