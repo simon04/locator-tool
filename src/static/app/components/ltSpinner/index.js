3a5f6da27f69ee5c575806d6cdad26d6bb340d12
Simon Legner <Simon.Legner@gmail.com>
2017-05-21 21:40:32 +0200

    ltSpinner: switch to progress-bar

--- src/static/app/components/ltSpinner/index.js
+++ src/static/app/components/ltSpinner/index.js
@@ -1,10 +1,5 @@
-import './ltSpinner.css';
+import template from './ltSpinner.pug';
 
 export default {
-  template: `
-  <div class="spinner">
-    <div class="bounce1"></div>
-    <div class="bounce2"></div>
-    <div class="bounce3"></div>
-  </div>`
+  template
 };
