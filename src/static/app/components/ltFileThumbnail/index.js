633f21dfa7451e21142f8add74b0e879fdbb33a6
Simon Legner <Simon.Legner@gmail.com>
2017-05-24 19:55:00 +0200

    Use precomputed widths for full screen image
    This improves loading performance since the image is precomputed on Commons.

--- src/static/app/components/ltFileThumbnail/index.js
+++ src/static/app/components/ltFileThumbnail/index.js
@@ -6,7 +6,14 @@ class controller {
   }
 
   get fullImageUrl() {
-    return this.file.imageUrl(this.$window.innerWidth);
+    const width = this.$window.innerWidth;
+    if (width >= 2048) {
+      return this.file.imageUrl(2048);
+    } else if (width >= 1280) {
+      return this.file.imageUrl(2048);
+    } else {
+      return this.file.imageUrl(1024);
+    }
   }
 }
 controller.$inject = ['$window'];
