ddbffdce971893e74e3d9ae4bd972e9363ebf27b
Simon Legner <Simon.Legner@gmail.com>
2018-03-14 17:38:50 +0100

    feat(i18n): add ml, ru languages

--- src/static/app/components/ltLanguageSelector/index.js
+++ src/static/app/components/ltLanguageSelector/index.js
@@ -4,17 +4,18 @@ import 'angular-gettext-loader!../../../po/bn.po';
 import 'angular-gettext-loader!../../../po/cs.po';
 import 'angular-gettext-loader!../../../po/de.po';
 import 'angular-gettext-loader!../../../po/fr.po';
+import 'angular-gettext-loader!../../../po/ml.po';
+import 'angular-gettext-loader!../../../po/ru.po';
+
+const languages = ['bn', 'cs', 'en', 'de', 'fr', 'ml', 'ru'];
 
 class ltLanguageSelector {
   constructor($window, localStorageService, gettext, gettextCatalog) {
     Object.assign(this, {$window, localStorageService, gettext, gettextCatalog});
-    this.languages = {
-      bn: this.getDisplayString('bn'),
-      cs: this.getDisplayString('cs'),
-      de: this.getDisplayString('de'),
-      en: 'English',
-      fr: this.getDisplayString('fr')
-    };
+    this.languages = languages.reduce((obj, lang) => {
+      obj[lang] = this.getDisplayString(lang);
+      return obj;
+    }, {});
     const language = localStorageService.get('language');
     if (language) {
       gettextCatalog.setCurrentLanguage(language);
@@ -36,6 +37,9 @@ class ltLanguageSelector {
   }
 
   getDisplayString(language) {
+    if (language === 'en') {
+      return 'English';
+    }
     const key = 'LANGUAGE';
     return this.gettextCatalog.getStringFormFor(language, key, 1) || language;
   }
