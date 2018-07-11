674f09f2892b26df561e63ecad03b7e6697a57f9
Simon Legner <Simon.Legner@gmail.com>
2018-07-11 09:23:24 +0200

    feat(i18n): add zh_TW language

--- src/static/app/components/ltLanguageSelector/index.js
+++ src/static/app/components/ltLanguageSelector/index.js
@@ -2,14 +2,15 @@ import template from './ltLanguageSelector.pug';
 
 import 'angular-gettext-loader!../../../po/bn.po';
 import 'angular-gettext-loader!../../../po/cs.po';
-import 'angular-gettext-loader!../../../po/es.po';
 import 'angular-gettext-loader!../../../po/de.po';
+import 'angular-gettext-loader!../../../po/es.po';
 import 'angular-gettext-loader!../../../po/fr.po';
 import 'angular-gettext-loader!../../../po/ml.po';
 import 'angular-gettext-loader!../../../po/pt.po';
 import 'angular-gettext-loader!../../../po/ru.po';
+import 'angular-gettext-loader!../../../po/zh_TW.po';
 
-const languages = ['bn', 'cs', 'en', 'es', 'de', 'fr', 'ml', 'pt', 'ru'];
+const languages = ['bn', 'cs', 'de', 'en', 'es', 'fr', 'ml', 'pt', 'ru', 'zh_TW'];
 
 class ltLanguageSelector {
   constructor($window, localStorageService, gettext, gettextCatalog) {
