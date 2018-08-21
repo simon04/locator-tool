891433a27605ad4023c5a684cf3af67ec7e820dc
Simon Legner <Simon.Legner@gmail.com>
2018-08-21 09:07:11 +0200

    refact(ltMap): use leaflet-providers

--- src/static/app/components/ltMap/index.js
+++ src/static/app/components/ltMap/index.js
@@ -26,51 +26,24 @@ class ltMap {
 
     map.attributionControl.setPrefix(false);
     const external = '<svg class="octicon"><use xlink:href="#link-external"></use></svg>';
-    const attribution = `<a href="https://www.openstreetmap.org/copyright" target="_blank">
-      OpenStreetMap</a> contributors`;
-    const wm = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-      name: 'Wikimedia maps',
-      maxZoom: 19,
-      attribution: 'Wikimedia maps | Map data &copy; ' + attribution
+    const wm = L.tileLayer.provider('Wikimedia', {
+      name: 'Wikimedia Maps'
     });
-    const osm = L.tileLayer('https://tiles.wmflabs.org/osm/{z}/{x}/{y}.png', {
+    const osm = L.tileLayer.provider('HikeBike', {
       name: 'OSM @wmflabs.org',
-      maxZoom: 18,
-      attribution
+      variant: 'osm'
     });
-    const osmOrg = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      name: `OSM ${external}`,
-      maxZoom: 19,
-      attribution
+    const osmOrg = L.tileLayer.provider('OpenStreetMap', {
+      name: `OSM ${external}`
     });
-    const basemap = L.tileLayer(
-      'https://maps{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg',
-      {
-        name: `basemap.at 🇦🇹 ${external}`,
-        subdomains: '1234',
-        maxZoom: 20,
-        attribution: `<a href="https://www.basemap.at/" target="_blank">basemap.at</a>
-        (<a href="https://creativecommons.org/licenses/by/3.0/at/deed.de" target="_blank">
-        CC-BY 3.0 AT</a>)`
-      }
-    );
-    const mapyCzBase = L.tileLayer('https://mapserver.mapy.cz/1base-m/{z}-{x}-{y}', {
-      name: `Mapy.cz base 🇨🇿 ${external}`,
-      maxZoom: 18,
-      attribution
+    const basemap = L.tileLayer.provider('BasemapAT.orthofoto', {
+      name: `basemap.at 🇦🇹 ${external}`
     });
-    const mapyCzPhoto = L.tileLayer('https://mapserver.mapy.cz/bing/{z}-{x}-{y}', {
-      name: `Mapy.cz Photo 🇨🇿 ${external}`,
-      maxZoom: 20,
-      attribution
-    });
-    const mapyCzTourism = L.tileLayer('https://mapserver.mapy.cz/1turist-m/{z}-{x}-{y}', {
-      name: `Mapy.cz Touristic map 🇨🇿 ${external}`,
-      maxZoom: 18,
-      attribution
+    const mapyCzPhoto = L.tileLayer.provider('mapyCZ', {
+      name: `Mapy.cz Photo 🇨🇿 ${external}`
     });
     const layersControl = L.control.layers().addTo(map);
-    const layers = [osmOrg, osm, wm, basemap, mapyCzBase, mapyCzPhoto, mapyCzTourism];
+    const layers = [osmOrg, osm, wm, basemap, mapyCzPhoto];
     layers.forEach(l => layersControl.addBaseLayer(l, l.options.name));
     const activeLayer = layers.filter(l => l.options.name === layerFromLocalStorage).shift() || osm;
     activeLayer.addTo(map);
--- src/static/app/vendor-leaflet.js
+++ src/static/app/vendor-leaflet.js
@@ -1,5 +1,17 @@
-import 'leaflet';
+import L from 'leaflet';
+import 'leaflet-providers';
 import 'leaflet-control-geocoder';
 
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.css';
+
+L.TileLayer.Provider.providers.mapyCZ = {
+  url: 'https://mapserver.mapy.cz/{variant}/{z}-{x}-{y}',
+  options: {
+    maxZoom: 20,
+    attribution: ['mapy.cz', 'seznam.cz', 'bing.com'].map(
+      url => ` <a href="https://${url}" target="_blank">${url}</a>`
+    ),
+    variant: 'bing'
+  }
+};
