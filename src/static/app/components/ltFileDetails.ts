bf05379479ca0d350894dea22c3ff184e967b017
Simon Legner <Simon.Legner@gmail.com>
2018-11-01 17:55:08 +0100

    refact(ltFileDetails): migrate to TypeScript

---
+++ src/static/app/components/ltFileDetails.ts
@@ -0,0 +1,31 @@
+import template from './ltFileDetails.pug';
+import LtDataAuth from '../api/ltDataAuth';
+import {CommonsFile} from '../model';
+
+class LtFileDetailsController implements ng.IComponentController {
+  error: any;
+  file: CommonsFile;
+
+  public static $inject = ['ltDataAuth'];
+  constructor(private ltDataAuth: LtDataAuth) {}
+
+  editLocation(coordinates) {
+    this.error = undefined;
+    return this.ltDataAuth.editLocation(this.file, coordinates).then(
+      () => {
+        coordinates.markAsSaved();
+      },
+      error => {
+        this.error = error;
+      }
+    );
+  }
+}
+
+export default {
+  bindings: {
+    file: '<'
+  },
+  controller: LtFileDetailsController,
+  template
+} as ng.IComponentOptions;
