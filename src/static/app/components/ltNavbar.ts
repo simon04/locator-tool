871c7847cb3810148e45f962f96d13ea0f501423
Simon Legner <Simon.Legner@gmail.com>
2018-11-01 19:22:47 +0100

    refact(ltNavbar): migrate to TypeScript

---
+++ src/static/app/components/ltNavbar.ts
@@ -0,0 +1,24 @@
+import template from './ltNavbar.pug';
+import {TransitionService} from '@uirouter/core';
+
+class LtNavbarController implements ng.IComponentController {
+  params: any;
+
+  public static $inject = ['$transitions'];
+  constructor(private $transitions: TransitionService) {}
+
+  $onInit() {
+    this.$transitions.onSuccess({}, transition => {
+      this.params = transition.params();
+    });
+  }
+
+  get activateLinks() {
+    return this.params && (this.params.files || this.params.user || this.params.category);
+  }
+}
+
+export default {
+  controller: LtNavbarController,
+  template
+} as ng.IComponentOptions;
