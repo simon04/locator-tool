a8f79e29dd6ce7b9385cb127eaecb3648c152ffd
Simon Legner <Simon.Legner@gmail.com>
2018-11-21 20:28:30 +0100

    fix(location_to_wikitext): numbered parameters

--- src/location_to_wikitext.py
+++ src/location_to_wikitext.py
@@ -32,17 +32,19 @@ def add_location_to_wikitext(type, lat, lng, wikitext):
     '{{Information}}{{Location|9.99|9.99|region:XY-Z}}{{Object location|12.3|45.6|region:XY-Z}}'
     >>> add_location_to_wikitext('Object location', 12.3, 45.6, "{{object location|87.65|-43.21|region:XY-Z}}")
     '{{Object location|12.3|45.6|region:XY-Z}}'
+    >>> add_location_to_wikitext('Location', 12.3, 45.6, "{{Location |1=47.27 |2=11.426944444444 }}")
+    '{{Location|12.3|45.6}}'
     """
     if type not in ['Location', 'Object location']:
         raise ValueError('Invalid location type')
     location = "{{%s|%s|%s}}" % (type, lat, lng)
-    numeric_arg = r'(\|\s*[-+.0-9]+\s*)'
+    numeric_arg = r'(\|\s*([1-9]\s*=\s*)?[-+.0-9]+\s*)'
     pattern = re.compile(
-        ('\{\{\s*%s\s*(' % type) + numeric_arg + '{3}\|\s*[NESW]\s*){2}', flags=re.IGNORECASE)
+        (r'\{\{\s*%s\s*(' % type) + numeric_arg + r'{3}\|\s*[NESW]\s*){2}', flags=re.IGNORECASE)
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
     pattern = re.compile(
-        ('\{\{\s*%s( dec)?\s*' % type) + numeric_arg + numeric_arg, flags=re.IGNORECASE)
+        (r'\{\{\s*%s( dec)?\s*' % type) + numeric_arg + numeric_arg, flags=re.IGNORECASE)
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
     pattern = re.compile(r'\{\{\s*Information', flags=re.IGNORECASE)
