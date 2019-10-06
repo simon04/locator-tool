f09e5fa1465d5848d16ddcc9fa6acbbc058e2ce0
Simon Legner <Simon.Legner@gmail.com>
2019-10-06 12:26:03 +0200

    build: drop core-js dependency
    No longer needed since migrating away from buble compiler.

--- src/static/app/vendor.ts
+++ src/static/app/vendor.ts
@@ -4,7 +4,6 @@ import 'angular-local-storage';
 import 'angular-gettext';
 import '@uirouter/angularjs';
 import 'angular-ll-leaflet';
-import 'core-js/fn/object/assign';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'octicons/build/octicons.css';
