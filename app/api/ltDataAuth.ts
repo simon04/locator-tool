b0d4e0cbbf95385b1b8d78828069ba6486b5e1a0
Simon Legner <Simon.Legner@gmail.com>
2021-01-31 23:50:21 +0100

    chore(ltFileDetails): error reporting

--- app/api/ltDataAuth.ts
+++ app/api/ltDataAuth.ts
@@ -26,7 +26,7 @@ export async function editLocation(title: CommonsFile, coordinates: LatLng): Pro
   // Reference: https://www.mediawiki.org/wiki/API:REST_API/Reference
   const pageUrl = `${API_URL}/v1/page/${title.file}`;
   const pageResponse = await fetch(pageUrl, {cache: 'no-cache'});
-  if (!pageResponse.ok) throw Error(pageResponse.statusText);
+  if (!pageResponse.ok) throw pageResponse;
   const page: Page = await pageResponse.json();
 
   const wikitext = addLocationToWikiText(coordinates, page.source);
@@ -43,20 +43,4 @@ export async function editLocation(title: CommonsFile, coordinates: LatLng): Pro
     })
   });
   if (!response.ok) throw Error(response.statusText);
-}
-
-function xsrfToken() {
-  return document.cookie
-    .split(';')
-    .find(item => item.trim().startsWith('XSRF-TOKEN='))
-    ?.trim()
-    ?.slice('XSRF-TOKEN='.length);
-}
-
-export function loginURL(): string {
-  return '/login?next=' + encodeURIComponent('/' + location.hash);
-}
-
-export function logoutURL(): string {
-  return '/logout?next=' + encodeURIComponent('/' + location.hash);
 }
