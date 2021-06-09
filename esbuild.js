6891b197998b4155c3ca75080ec1aedf957dc178
Simon Legner <Simon.Legner@gmail.com>
2021-06-09 21:21:11 +0200

    chore(build): add hash as query parameter

--- esbuild.js
+++ esbuild.js
@@ -4,8 +4,11 @@
 function git(command) {
   return require('child_process').execSync(`git ${command}`, {encoding: 'utf8'}).trim();
 }
+const __BUILD_DATE__ = git('log -1 --format=%cd --date=iso');
+const __BUILD_VERSION__ = git('describe --always');
 
 const esbuild = require('esbuild');
+const fs = require('fs');
 
 /**
  * @type {esbuild.BuildOptions}
@@ -20,8 +23,8 @@ const options = {
     '.svg': 'text'
   },
   define: {
-    __BUILD_DATE__: JSON.stringify(git('log -1 --format=%cd --date=iso')),
-    __BUILD_VERSION__: JSON.stringify(git('describe --always'))
+    __BUILD_DATE__: JSON.stringify(__BUILD_DATE__),
+    __BUILD_VERSION__: JSON.stringify(__BUILD_VERSION__)
   },
   outdir: 'dist/',
   sourcemap: true,
@@ -32,4 +35,9 @@ if (process.argv.includes('serve')) {
   esbuild.serve({servedir: 'dist/'}, options).catch(() => process.exit(1));
 } else {
   esbuild.build(options).catch(() => process.exit(1));
+  const index = 'dist/index.html';
+  const html = fs
+    .readFileSync(index, {encoding: 'utf8'})
+    .replace(/"(index.css|index.js)[^"]*"/g, `"$1?${__BUILD_VERSION__}"`);
+  fs.writeFileSync(index, html, {encoding: 'utf8'});
 }
