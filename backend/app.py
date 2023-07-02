16872804f3bc59f0f2009dc0cb520d45a7fc2b51
Simon Legner <Simon.Legner@gmail.com>
2023-07-02 09:35:48 +0200

    style: reformat Python code using black

--- backend/app.py
+++ backend/app.py
@@ -6,92 +6,108 @@ from oauthlib.common import to_unicode
 
 import logging
 import os
+
 logging.basicConfig(
-    filename=os.path.join(os.path.dirname(__file__), 'locator-tool.log'),
-    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    filename=os.path.join(os.path.dirname(__file__), "locator-tool.log"),
+    format="[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
     level=logging.DEBUG,
 )
 
-app = Flask(__name__, static_url_path='', static_folder='static/')
-app.config.from_pyfile('config.py')
+app = Flask(__name__, static_url_path="", static_folder="static/")
+app.config.from_pyfile("config.py")
 
 # HTTP security headers
 Talisman(app, content_security_policy={})
 
 # CSRF protection. settings fitting Angular $httpProvider
-app.config['CSRF_COOKIE_NAME'] = 'XSRF-TOKEN'
-app.config['CSRF_HEADER_NAME'] = 'X-XSRF-TOKEN'
-app.config['CSRF_COOKIE_PATH'] = '/'
+app.config["CSRF_COOKIE_NAME"] = "XSRF-TOKEN"
+app.config["CSRF_HEADER_NAME"] = "X-XSRF-TOKEN"
+app.config["CSRF_COOKIE_PATH"] = "/"
 SeaSurf(app)
 
-mwoauth = MWOAuth(base_url='https://commons.wikimedia.org/w',
-                  clean_url='https://commons.wikimedia.org/wiki',
-                  consumer_key=app.config['OAUTH_CONSUMER_KEY'],
-                  consumer_secret=app.config['OAUTH_CONSUMER_SECRET'])
+mwoauth = MWOAuth(
+    base_url="https://commons.wikimedia.org/w",
+    clean_url="https://commons.wikimedia.org/wiki",
+    consumer_key=app.config["OAUTH_CONSUMER_KEY"],
+    consumer_secret=app.config["OAUTH_CONSUMER_SECRET"],
+)
 app.register_blueprint(mwoauth.bp)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
 
 
-@app.route('/user')
+@app.route("/user")
 def user():
     r = jsonify(user=mwoauth.get_current_user(False))
     return r
 
 
-@app.route('/edit', methods=['POST'])
+@app.route("/edit", methods=["POST"])
 def edit():
     if not mwoauth.get_current_user():
         abort(401)
-    data = request.get_json()
-    if 'pageid' not in data or 'type' not in data \
-            or 'lat' not in data or 'lng' not in data:
-        abort(400)
-    pageid = int(data['pageid'])
-    type = data['type']
-    lat = float(data['lat'])
-    lng = float(data['lng'])
-    app.logger.info('Received request %s', str(data))
 
-    r1 = mwoauth_request({
-        'action': 'query',
-        'pageids': pageid,
-        'prop': 'revisions',
-        'rvprop': 'content',
-        'meta': 'tokens'
-    })
+    data = request.get_json()
+    if (
+        "pageid" not in data
+        or "type" not in data
+        or "lat" not in data
+        or "lng" not in data
+    ):
+        abort(400)
+
+    pageid = int(data["pageid"])
+    type = data["type"]
+    lat = float(data["lat"])
+    lng = float(data["lng"])
+    app.logger.info("Received request %s", str(data))
+
+    r1 = mwoauth_request(
+        {
+            "action": "query",
+            "pageids": pageid,
+            "prop": "revisions",
+            "rvprop": "content",
+            "meta": "tokens",
+        }
+    )
     try:
-        wikitext = r1['query']['pages'][str(pageid)]['revisions'][0]['*']
+        wikitext = r1["query"]["pages"][str(pageid)]["revisions"][0]["*"]
         wikitext = to_unicode(wikitext)
     except KeyError:
         abort(404)
+
     try:
-        token = r1['query']['tokens']['csrftoken']
+        token = r1["query"]["tokens"]["csrftoken"]
     except KeyError:
         abort(401)
-    app.logger.info('Obtained token=%s for pageid=%d', token, pageid)
+
+    app.logger.info("Obtained token=%s for pageid=%d", token, pageid)
 
     from location_to_wikitext import add_location_to_wikitext
+
     new_wikitext = add_location_to_wikitext(type, lat, lng, wikitext)
 
-    r2 = mwoauth_request({
-        'action': 'edit',
-        'pageid': str(pageid),
-        'summary': '{{%s}}' % type,
-        'text': new_wikitext,
-        'token': token
-    })
+    r2 = mwoauth_request(
+        {
+            "action": "edit",
+            "pageid": str(pageid),
+            "summary": "{{%s}}" % type,
+            "text": new_wikitext,
+            "token": token,
+        }
+    )
 
     return jsonify(result=r2)
 
 
 def mwoauth_request(api_query):
-    api_query['format'] = 'json'
-    return mwoauth.mwoauth.post(mwoauth.base_url + '/api.php?', data=api_query).data
+    api_query["format"] = "json"
+    return mwoauth.mwoauth.post(mwoauth.base_url + "/api.php?", data=api_query).data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- backend/location_to_wikitext.py
+++ backend/location_to_wikitext.py
@@ -37,30 +37,33 @@ def add_location_to_wikitext(type, lat, lng, wikitext):
     >>> add_location_to_wikitext('Location', 12.3, 45.6, "{{Location |1=47.27 |2=11.426944444444 }}")
     '{{Location|12.3|45.6}}'
     """
-    if type not in ['Location', 'Object location']:
-        raise ValueError('Invalid location type')
-    pattern = re.compile(r'\{\{\s*Location possible\s*\}\}', flags=re.IGNORECASE)
-    wikitext = pattern.sub('', wikitext)
+    if type not in ["Location", "Object location"]:
+        raise ValueError("Invalid location type")
+    pattern = re.compile(r"\{\{\s*Location possible\s*\}\}", flags=re.IGNORECASE)
+    wikitext = pattern.sub("", wikitext)
     location = "{{%s|%s|%s}}" % (type, lat, lng)
-    numeric_arg = r'(\|\s*([1-9]\s*=\s*)?[-+.0-9]+\s*)'
+    numeric_arg = r"(\|\s*([1-9]\s*=\s*)?[-+.0-9]+\s*)"
     pattern = re.compile(
-        (r'\{\{\s*%s\s*(' % type) + numeric_arg + r'{3}\|\s*[NESW]\s*){2}', flags=re.IGNORECASE)
+        (r"\{\{\s*%s\s*(" % type) + numeric_arg + r"{3}\|\s*[NESW]\s*){2}",
+        flags=re.IGNORECASE,
+    )
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
     pattern = re.compile(
-        (r'\{\{\s*%s( dec)?\s*' % type) + numeric_arg + numeric_arg, flags=re.IGNORECASE)
+        (r"\{\{\s*%s( dec)?\s*" % type) + numeric_arg + numeric_arg, flags=re.IGNORECASE
+    )
     if pattern.search(wikitext):
         return pattern.sub(location[:-2], wikitext, count=1)
-    pattern = re.compile(r'\{\{\s*Information', flags=re.IGNORECASE)
+    pattern = re.compile(r"\{\{\s*Information", flags=re.IGNORECASE)
     match = pattern.search(wikitext)
     if match:
         brace_count = 2
         pos = match.start() + 2
         while brace_count > 0 and pos < len(wikitext):
-            if wikitext[pos] == '{':
+            if wikitext[pos] == "{":
                 brace_count += 1
-            elif wikitext[pos] == '}':
+            elif wikitext[pos] == "}":
                 brace_count -= 1
             pos += 1
-        return '\n'.join([wikitext[:pos], location, wikitext[pos:]])
+        return "\n".join([wikitext[:pos], location, wikitext[pos:]])
     return wikitext + location
