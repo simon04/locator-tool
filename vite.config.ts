56b2c1f5e95cab0cd2077602ef89a7b1ab33728c
Simon Legner <Simon.Legner@gmail.com>
2024-02-04 16:23:35 +0100

    build: chunkFileNames

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,13 @@ process.env.VITE_APP_DEPENDENCIES = JSON.stringify(
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {sourcemap: true},
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[hash:19].js'
+      }
+    }
+  },
   plugins: [vue()]
 });
