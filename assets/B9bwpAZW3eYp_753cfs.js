20b852c3deca9d3b9e7d129d54ca44e36bd81f8f
simon04 <simon04@users.noreply.github.com>
2024-09-05 16:40:34 +0000

    deploy: b09d451d32b603a22d47881464123dcf2bec210b

---
+++ assets/B9bwpAZW3eYp_753cfs.js
@@ -0,0 +1,6 @@
+import{d as l,j as p,t as c,I as f,c as u,y as m,o as d}from"./index-C16W2vvD.js";import{u as _,L as g}from"./B7RvLhaUxq88ZPtlkam.js";import{b as k,c as y}from"./DadgABpvQcQp3i1OjiC.js";import{u as b,r as h}from"./CDkn4XL4aAZRSkDHSiE.js";const B=l({__name:"ltAllMap",setup(M){const s=m(),t=p(null);return b(h(),c("Map")),f(async()=>{const{map:a}=_(t),o=await k(s.query),r=(await y(o)).flatMap(e=>{if(!e.coordinates.isDefined)return[];const{lat:n,lng:i}=e.coordinates;return[g.circleMarker({lat:n,lng:i},{color:"#2a4b8d"}).bindTooltip(e.file).bindPopup(()=>`
+<div style="width: 300px">
+  <p><a href="${e.url}" target="_blank">${e.file}</a></p>
+  <img src="${e.imageUrl(300)}" style="width: 100%" />
+</div>`).addTo(a).getLatLng()]});a.fitBounds(r)}),(a,o)=>(d(),u("div",{ref_key:"mapRef",ref:t,class:"map fill",style:{"min-height":"300px"}},null,512))}});export{B as default};
+//# sourceMappingURL=B9bwpAZW3eYp_753cfs.js.map
---
+++ assets/jL5HkdU8t9i2Y2c9S2d.js
@@ -0,0 +1,2 @@
+import{o as n,c,a}from"./index-C16W2vvD.js";const i=(s,r)=>{const e=s.__vccOpts||s;for(const[t,o]of r)e[t]=o;return e},l={},p={class:"progress"};function _(s,r){return n(),c("div",p,r[0]||(r[0]=[a("div",{class:"progress-bar progress-bar-info progress-bar-striped active",style:{width:"100%"}},null,-1)]))}const f=i(l,[["render",_]]);export{f as l};
+//# sourceMappingURL=jL5HkdU8t9i2Y2c9S2d.js.map
