37bfaf77748f92e2670bb095cf2c1e4918a7f551
Simon Legner <Simon.Legner@gmail.com>
2026-03-13 10:05:56 +0100

    fix(ltGallery): :key=

--- app/components/ltGallery.vue
+++ app/components/ltGallery.vue
@@ -30,7 +30,7 @@
   </div>
 
   <div class="mt-3 row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-4">
-    <div v-for="title in sortedTitles" :key="title" class="col">
+    <div v-for="title in sortedTitles" :key="title.file" class="col">
       <lt-gallery-card :title="title" />
     </div>
   </div>
